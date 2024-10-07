4cbe81ced5e8633e32a06c1d94c08f9daad44d6c
avraham <2721avi@gmail.com>
2024-10-07 10:31:32 +0300

    create conrct empty routers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const app = exp();
 dotenv.config();
 const port = process.env.PORT || 9999;
 
+app.use(exp.json());
+app.use("auth", require("./routers/authRuoter"));
+app.use("/user", require("./routers/userRout"));
+app.use("/greeneye", require("./routers/greenEyeRouter"));
+
 app.listen(port, () => {
   console.log(port, `runing in ${port}`);
 });
