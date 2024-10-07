9bcf40c36c0d96c63ac3e15faf499b9c28833408
avraham <2721avi@gmail.com>
2024-10-07 10:16:30 +0300

    basic index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+const exp = require("express");
+const dotenv = require("dotenv");
+const app = exp();
+dotenv.config();
+const port = process.env.PORT || 9999;
 
+app.listen(port, () => {
+  console.log(port, `runing in ${port}`);
+});
