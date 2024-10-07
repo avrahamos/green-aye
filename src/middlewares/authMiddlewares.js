b23a94844215f4884019dc4ff786a18ed5701dfb
avraham <2721avi@gmail.com>
2024-10-07 17:09:17 +0300

    middelware done

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -1,10 +1,26 @@
+const jwt = require("crypto");
+
 const onlyCommanders = async (req, res, next) => {
   try {
+    const token = req.cookies.token;
+    const userData = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = userData;
+    if (userData.role !== "commander") {
+      res.status(403).send("shtzchhhhh.....");
+    }
+    next();
   } catch (eror) {}
 };
 const onlySoldiersAndCommanders = async (req, res, next) => {
   try {
-  } catch (eror) {}
+    const token = req.cookies.token;
+    const userData = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = userData;
+    next();
+  } catch (eror) {
+    console.log(eror);
+    res.status(401).send(eror.messege);
+  }
 };
 
 module.exports = {
